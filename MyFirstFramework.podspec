187abdf6608a80b0e48be47340be8aba0384b00f
朱佳男 <zhujianan@aiyunxiao.com>
2021-05-10 15:49:31 +0800

    diercitijiao

--- MyFirstFramework.podspec
+++ MyFirstFramework.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "MyFirstFramework"
-  spec.version      = "0.0.1"
+  spec.version      = "1.0.0"
   spec.summary      = "A short description of MyFirstFramework."
 
   # This description is used to generate tags and improve search results.
---
+++ MyFirstFramework/MyFirstFramework/MyFirstFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstFramework.h
+//  MyFirstFramework
+//
+//  Created by zhujianan on 2021/5/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstFramework.
+FOUNDATION_EXPORT double MyFirstFrameworkVersionNumber;
+
+//! Project version string for MyFirstFramework.
+FOUNDATION_EXPORT const unsigned char MyFirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramework/PublicHeader.h>
+
+
