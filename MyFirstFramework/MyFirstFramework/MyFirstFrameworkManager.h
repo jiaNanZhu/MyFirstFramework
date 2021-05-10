fb26096daede4250cbcccf1a0ccb7e37771a84b7
朱佳男 <zhujianan@aiyunxiao.com>
2021-05-10 16:15:41 +0800

    diercitijiao

---
+++ MyFirstFramework/MyFirstFramework/MyFirstFrameworkManager.h
@@ -0,0 +1,16 @@
+//
+//  MyFirstFrameworkManager.h
+//  MyFirstFramework
+//
+//  Created by zhujianan on 2021/5/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyFirstFrameworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyFirstFramework/MyFirstFramework/MyFirstFrameworkManager.m
@@ -0,0 +1,12 @@
+//
+//  MyFirstFrameworkManager.m
+//  MyFirstFramework
+//
+//  Created by zhujianan on 2021/5/10.
+//
+
+#import "MyFirstFrameworkManager.h"
+
+@implementation MyFirstFrameworkManager
+
+@end
